41703ca41b087896012beeb3f6d5dc6a1d0f9ba0
Cihat Turgut <cihatturgut@yaani.com>
2025-01-30 14:02:34 +0300

    Completed - Header Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "serif"],
+        open: ["Open Sans", "serif"],
+      },
+      colors: {
+        "cineverse-black": "#111",
+        "cineverse-pencil-black": "#27272C",
+        "cineverse-red": "#BC1A45",
+        "cineverse-melon": "#FFD369",
+        "cineverse-grey": "#DDDDDD",
+        "cineverse-white": "#F7F7F7",
+        "cineverse-light-blue": "#45aaf2",
+        "cineverse-dark-blue": "#2e9add",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".text-shadow": {
+          textShadow:
+            "0 0 5px rgb(69, 170, 242), 0 0 10px rgb(69, 170, 242), 0 0 15px rgb(69, 170, 242)",
+        },
+        ".group-hover\\:text-shadow:hover": {
+          textShadow:
+            "0 0 5px rgb(69, 170, 242), 0 0 10px rgb(69, 170, 242), 0 0 15px rgb(69, 170, 242)",
+        },
+      });
+    },
+  ],
 };
